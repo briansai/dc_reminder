7a03dea3451a64e7b9909a907e1568edf77d716a
Brian Sai <brianwsai@gmail.com>
2021-09-10 17:05:50 -0700

    Scheduled the function to run every two weeks

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const sgMail = require('@sendgrid/mail');
 const cron = require('node-cron');
 const moment = require('moment');
 const dotenv = require('dotenv');
+const { default: axios } = require('axios');
 const app = express();
 
 require('moment-recur');
@@ -10,50 +11,45 @@ dotenv.config();
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-cron.schedule('0 4 * * 2', () => {
-  const interval = moment(new Date('09/07/2021')).recur().every(2).weeks();
+cron.schedule(
+  '0 4 * * 2',
+  () => {
+    const interval = moment(new Date('9/7/2021')).recur().every(2).weeks();
 
-  if (interval.matches(new Date())) {
-    const endPeriod = moment().format('M-D-YY');
+    if (interval.matches(new Date())) {
+      const endPeriod = moment().format('M-D-YY');
 
-    const msg = {
-      to: process.env.EMPLOYEE1,
-      from: process.env.SELF,
-      subject: 'Aquatics Timesheet Reminder',
-      text: `Hello everyone,\n\nThe timesheet ending in ${endPeriod} is due by tonight.  Thank you.\n\nBest,\nBrian`,
-    };
-
-    sgMail
-      .send(msg)
-      .then(() => {
-        console.log('Email sent');
-      })
-      .catch((error) => {
-        console.error(error.response.body);
-      });
+      axios
+        .get('https://api.sendgrid.com/v3/marketing/contacts', {
+          headers: { Authorization: `Bearer ${process.env.SENDGRID_API_KEY}` },
+        })
+        .then((res) => {
+          res.data.result.forEach((employee) => {
+            const { email, first_name } = employee;
+            sgMail
+              .send({
+                to: email,
+                from: process.env.SELF,
+                subject: 'Aquatics Timesheet Reminder',
+                text: `Hello ${first_name},\n\nThe timesheet ending in ${endPeriod} is due by tonight.  Thank you.\n\nBest,\nBrian`,
+              })
+              .then(() => {
+                console.log(`Email sent to: ${first_name}`);
+              })
+              .catch((error) => {
+                console.error(error.response.body);
+              });
+          });
+        })
+        .catch((err) => console.error(err.response.data));
+    }
+  },
+  {
+    scheduled: true,
+    timezone: 'America/Los_Angeles',
   }
-});
+);
 
 app.listen(process.env.PORT, () => {
   console.log(`Connected to Port: ${process.env.PORT}`);
 });
-
-// const promises = employees.map((employee) =>
-//   sgMail
-//     .send({
-//       to: employee,
-//       from: process.env.SELF,
-//       subject: 'Aquatics Timesheet Reminders',
-//       text: 'Hello staff,\n\nThis is Brian, your Senior Guard.  You will be receiving email reminders every two weeks from this email so it relieves Chris from having to send you a reminder.  This scheduled reminder is automated, so please do not respond to this email directly.  If you have any questions, please reach out to Chris or send me a text.  Thanks!\n\nBest,\nBrian',
-//     })
-//     .then(() => {
-//       console.log('Email sent');
-//     })
-//     .catch((error) => {
-//       console.error(error.response.body);
-//     })
-// );
-
-// Promise.all(promises)
-//   .then(() => console.log('ALL DONE!'))
-//   .catch((err) => console.error(err));
